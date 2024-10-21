4b1f3fa67663ade9833e8c0580d683d4e88e90ab
Pawel <p.awel608@wp.pl>
2024-10-21 22:33:58 +0200

    history of moves added

--- src/components/Puzzles/DailyPuzzle/AfterClick/DisplayDailyPuzzle.style.ts
+++ src/components/Puzzles/DailyPuzzle/AfterClick/DisplayDailyPuzzle.style.ts
@@ -1,7 +1,29 @@
 import { SxProps } from "@mui/material";
 
 export const Chessboard: SxProps = {
-  width: "40%",
+  width: "60%",
   height: "auto",
   border: "1px solid black",
+};
+export const Main: SxProps = {
+  backgroundColor: "#312e2b",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "30px",
+};
+
+export const Moves: SxProps = {
+  backgroundColor: "#242424",
+  color: "white",
+  height: "auto",
+  padding: "20px",
+};
+
+export const Title: SxProps = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px",
 };
--- src/components/Puzzles/DailyPuzzle/AfterClick/DisplayDailyPuzzle.tsx
+++ src/components/Puzzles/DailyPuzzle/AfterClick/DisplayDailyPuzzle.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
-import { Box } from "@mui/material";
+import { Box, List, ListItem, Typography } from "@mui/material";
+import ExtensionIcon from "@mui/icons-material/Extension";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 import { Piece, Square } from "react-chessboard/dist/chessboard/types";
 import * as style from "./DisplayDailyPuzzle.style";
 import { Puzzle } from "../PuzzleTypes";
@@ -12,6 +15,9 @@ export default function Puzzles({ puzzle }: { puzzle: Puzzle | null }) {
   const [fen, setFen] = useState<string>("start");
   const [moves, setMoves] = useState<string[]>([]);
   const [currentMoveIndex, setCurrentMoveIndex] = useState<number>(0);
+  const [playedMoves, setPlayedMoves] = useState<
+    { move: string; isValid: boolean }[]
+  >([]);
 
   const cleanMove = (move: string): string | null => {
     // Usuwa numer ruchu, "x", "+", "!" i inne znaki, aby uzyskać czysty ruch
@@ -84,24 +90,30 @@ export default function Puzzles({ puzzle }: { puzzle: Puzzle | null }) {
       promotion: "q",
     });
 
+    const moveDescription = move.san;
+
     if (move === null) {
       console.error(`Invalid move: ${sourceSquare} to ${targetSquare}`);
       setFen(previousFen);
+
       return false;
     }
 
     console.log("Move performed:", move.san);
 
     const sanitizedMove = cleanMove(move.san);
-
     const moveIndex = currentMoveIndex;
 
     console.log("Sanitized Move:", sanitizedMove);
     console.log("Expected Move (White):", moves[moveIndex]);
 
     if (sanitizedMove === moves[moveIndex]) {
-      console.log("Dobry ruch białych:", move.san);
+      setPlayedMoves((prevMoves) => [
+        ...prevMoves,
+        { move: move.san, isValid: true },
+      ]);
 
+      console.log("Dobry ruch białych:", move.san);
       setCurrentMoveIndex(moveIndex + 1);
 
       const blackMoveIndex = moveIndex + 1;
@@ -118,7 +130,6 @@ export default function Puzzles({ puzzle }: { puzzle: Puzzle | null }) {
         }
       }
 
-      //Czy koniec ruchów
       if (blackMoveIndex + 1 === moves.length) {
         setIsMovable(false);
         console.log("Wszystkie ruchy wykonane. Ruchy zostały zablokowane.");
@@ -127,6 +138,11 @@ export default function Puzzles({ puzzle }: { puzzle: Puzzle | null }) {
       console.error("Zły ruch białych:", move.san);
       chess.undo();
       setFen(previousFen);
+
+      setPlayedMoves((prevMoves) => [
+        ...prevMoves,
+        { move: moveDescription, isValid: false },
+      ]);
     }
 
     setFen(chess.fen());
@@ -135,15 +151,83 @@ export default function Puzzles({ puzzle }: { puzzle: Puzzle | null }) {
   };
 
   return (
-    <Box>
-      <Chessboard
-        id="BasicChessboard"
-        position={fen}
-        onPieceDrop={onPieceDrop}
-        arePiecesDraggable={isMovable}
-        customDarkSquareStyle={{ backgroundColor: "#e0e0e0" }}
-        customLightSquareStyle={{ backgroundColor: "#607d8b" }}
-      />
+    <Box sx={style.Main}>
+      <Box sx={style.Chessboard}>
+        <Chessboard
+          id="BasicChessboard"
+          position={fen}
+          onPieceDrop={onPieceDrop}
+          arePiecesDraggable={isMovable}
+          customDarkSquareStyle={{ backgroundColor: "#e0e0e0" }}
+          customLightSquareStyle={{ backgroundColor: "#607d8b" }}
+        />
+      </Box>
+      <Box sx={style.Moves}>
+        <Box sx={style.Title}>
+          <Typography>Daily Puzzle</Typography>
+          <ExtensionIcon sx={{ color: "white" }} />
+        </Box>
+        <List>
+          {playedMoves.map((moveName, index) => (
+            <ListItem key={index}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  backgroundColor: "#FFFFFF1A",
+                  padding: "15px",
+                  borderRadius: "10px",
+                  width: "100%",
+                }}
+              >
+                {moveName.isValid ? (
+                  <CheckCircleIcon
+                    sx={{
+                      color: "#81B64C",
+                      marginRight: 1,
+                      backgroundColor: "",
+                    }}
+                  />
+                ) : (
+                  <CancelIcon sx={{ color: "red", marginRight: 1 }} />
+                )}
+                <Typography
+                  sx={
+                    moveName.isValid
+                      ? {
+                          color: "#81B64C",
+                        }
+                      : { color: "#FA412D" }
+                  }
+                >
+                  {moveName.move}
+                </Typography>
+                {moveName.isValid ? (
+                  <Typography
+                    sx={{
+                      color: "#81B64C",
+                      marginLeft: 1,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    is correct!
+                  </Typography>
+                ) : (
+                  <Typography
+                    sx={{
+                      color: "#FA412D",
+                      marginLeft: 1,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    is not correct. Try again!
+                  </Typography>
+                )}
+              </Box>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
     </Box>
   );
 }
