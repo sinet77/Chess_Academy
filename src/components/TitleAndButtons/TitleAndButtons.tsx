c8bc06a827e97f05ee7a0ee72f1abe4a3bd9d1a3
Paweł Kozłowski <p.awel608@wp.pl>
2024-11-21 21:15:41 +0100

    added tile with pawns on main page

--- src/components/TitleAndButtons/TitleAndButtons.tsx
+++ src/components/TitleAndButtons/TitleAndButtons.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Link, Typography } from "@mui/material";
 import * as style from "./TitleAndButtons.style";
 import { motion } from "framer-motion";
-import { training, computer, puzzle } from "../../assets/MainPageImages.ts";
+import { training, computer, puzzle, pawns } from "../../assets/MainPageImages.ts";
 import { Link as RouterLink } from "react-router-dom";
 import { routes } from "../../routes.ts";
 import DailyPuzzleHomePage from "../Puzzles/DailyPuzzle/OnMainPage/DailyPuzzleHomePage.tsx";
@@ -64,6 +64,16 @@ export default function TitleAndButtons() {
             <Button sx={style.ImageButtonContainer}>
               <img src={puzzle} alt="Chess Puzzle" style={style.ButtonImage} />
               <Typography sx={style.TitleUnderButtonImage}>Puzzles</Typography>
+            </Button>
+          </Link>
+          <Link
+            to={routes.chooseStartingPosition}
+            component={RouterLink}
+            sx={style.TextUnderlineTurnOff}
+          >
+            <Button sx={style.ImageButtonContainer}>
+              <img src={pawns} alt="Game with pawns only" style={style.ButtonImage} />
+              <Typography sx={style.TitleUnderButtonImage}>Pawns Game</Typography>
             </Button>
           </Link>
           <DailyPuzzleHomePage />
