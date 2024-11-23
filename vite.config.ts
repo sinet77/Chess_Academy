f9b981d18aedaa86868105c6763c3296cabf2b6a
Pawel <p.awel608@wp.pl>
2024-11-23 17:49:22 +0100

    add: base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Chess_Academy/",
+  base: "./Chess_Academy/",
   optimizeDeps: {
     include: [
       "@mui/material/Tooltip",
