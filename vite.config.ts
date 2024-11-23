cb1bd101d9a5b2e19c4692e3d1b9931fc7791c1d
Pawel <p.awel608@wp.pl>
2024-11-23 18:13:48 +0100

    fix: react plugin added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { ghPages } from "vite-plugin-gh-pages";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ghPages()],
+  plugins: [react(), ghPages()],
   base: "/Chess_Academy",
   optimizeDeps: {
     include: [
