c5f0a41f206350636779013c7a26f4275f0bddd8
Paweł Kozłowski <p.awel608@wp.pl>
2024-11-27 16:44:08 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
-import { ghPages } from "vite-plugin-gh-pages";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/Chess_Academy",
+  base: '/Chess_Academy/',
+  plugins: [react()],
   optimizeDeps: {
     include: [
       "@mui/material/Tooltip",
