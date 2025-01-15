58c0df52f9cc72fc293a8804e106a68c25dd637e
Paweł Kozłowski <p.awel608@wp.pl>
2025-01-15 19:20:37 +0100

    add: starting connecting colors to firebase

--- src/components/ColorsChessboard/ColorsChessboard.tsx
+++ src/components/ColorsChessboard/ColorsChessboard.tsx
@@ -1,16 +1,34 @@
 import { Box } from "@mui/material";
+import { useColorsChessboard } from "./useColorsChessboard";
 
 export const ColorsChessboard = () => {
-  return (
-    <Box>
-      <label>
-        Dark square{" "}
-        <input type="color" />
-      </label>
-      <label>
-        Light square{" "}
-        <input type="color" />
-      </label>
-    </Box>
-  );
-};
+  const { colors, updateColors } = useColorsChessboard();
+
+  const handleColorChange = (
+    field: "darkSquare" | "lightSquare",
+    value: string
+  ) => {
+    updateColors({ [field]: value });
+  }
+    return (
+      <Box>
+        <label>
+          Dark square{" "}
+          <input
+            type="color"
+            value={colors?.darkSquare}
+            onChange={(e) => handleColorChange("darkSquare", e.target.value)}
+          />
+        </label>
+        <label>
+          Light square{" "}
+          <input
+            type="color"
+            value={colors?.lightSquare}
+            onChange={(e) => handleColorChange("lightSquare", e.target.value)}
+          />
+        </label>
+      </Box>
+    );
+  };
+
---
+++ src/components/ColorsChessboard/useColorsChessboard.ts
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { doc, getDoc,updateDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase.ts";
+import { useAuth } from "../../context/authContext/index.tsx";
+
+type ChessboardColors = {
+  darkSquare: string;
+  lightSquare: string;
+};
+
+export const useColorsChessboard = () => {
+  const { currentUser } = useAuth();
+  const [colors, setColors] = useState<ChessboardColors | null>(null);
+
+  useEffect(() => {
+    const fetchColors = async () => {
+      if (!currentUser) return
+        const chessboardColorsRef = doc(
+          db,
+          "Users",
+          currentUser.uid,
+          "chessboard",
+          "chessboardDocId"
+        );
+        try {
+          const chessboardColorsDocSnapshot = await getDoc(chessboardColorsRef);
+  
+          if (chessboardColorsDocSnapshot.exists()) {
+            const data = chessboardColorsDocSnapshot.data() as ChessboardColors;
+            setColors(data);
+          } else {
+            console.error("Nie znaleziono dokumentu z kolorami w bazie danych.");
+          }
+        } catch (error) {
+          console.error("Błąd podczas pobierania kolorów:", error);
+        }
+      };
+  
+      fetchColors();
+    }, [currentUser]);
+
+    const updateColors = async (newColors: Partial<ChessboardColors>) => {
+      if (!currentUser || !colors) return;
+  
+      const chessboardColorsRef = doc(
+        db,
+        "Users",
+        currentUser.uid,
+        "chessboard",
+        "FEAAmh0EJsmR1BLB5jwV"
+      );
+  
+      try {
+        await updateDoc(chessboardColorsRef, newColors);
+        setColors((prevColors) => ({ ...prevColors!, ...newColors }));
+      } catch (error) {
+        console.error("Błąd podczas aktualizacji kolorów:", error);
+      }
+    };
+  
+    return { colors, updateColors };
+  };
+
+
+
